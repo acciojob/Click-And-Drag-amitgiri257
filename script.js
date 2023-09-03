072dc53a16f8edaa7ed2ed19c37b07d508781966
amitgiri257 <127419759+amitgiri257@users.noreply.github.com>
2023-09-03 09:23:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Your code here.
+const line = document.getElementById('line');
+let rotation = 0;
+ 
+setInterval(() => {
+  rotation += 5;
+  line.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+}, 50);
